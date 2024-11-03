89211f5a553ee1629dcfd7b18b9dade0bcdfe7ed
Bativolt <camoglu.g@live.be>
2024-11-03 19:52:56 +0100

    google analytics code

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,14 +51,10 @@ const config = {
   
   scripts: [
     {
-      type: 'text/javascript',
-      innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-K6L28WB5');`,
+      src: '/gtm.js', // Chemin relatif du fichier dans le dossier static
+      async: true,
     },
-  ],
+  ],  
   
   themeConfig: {
     algolia: {
