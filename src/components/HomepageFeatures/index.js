911b5f34a38b37508889d0026e1cd6cca72624ba
Bativolt <camoglu.g@live.be>
2024-11-08 23:45:18 +0100

    trasnlations

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,66 +4,67 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Accès Simple et Intuitif aux Normes RGIE',
+    title: <translate>Accès Simple et Intuitif aux Normes RGIE</translate>,
     Svg: require('@site/static/img/docs_bativolt_browsing_re_eycn.svg').default,
-    altText: 'Image représentant un accès simple aux normes RGIE',
+    altText: <translate>Image représentant un accès simple aux normes RGIE</translate>,
     description: (
       <>
-        Découvrez un accès facile aux ressources du RGIE, structurées de manière claire et accessible. Notre interface est conçue pour vous permettre de naviguer efficacement dans les exigences de conformité électrique, que vous soyez novice ou professionnel.
+        <translate>Découvrez un accès facile aux ressources du RGIE, structurées de manière claire et accessible. Notre interface est conçue pour vous permettre de naviguer efficacement dans les exigences de conformité électrique, que vous soyez novice ou professionnel.</translate>
       </>
     ),
   },
   {
-    title: 'Guide Complet pour la Conformité Électrique',
+    title: <translate>Guide Complet pour la Conformité Électrique</translate>,
     Svg: require('@site/static/img/docs_bativolt_goals_re_lu76.svg').default,
-    altText: 'Image représentant un guide complet pour la conformité électrique',
+    altText: <translate>Image représentant un guide complet pour la conformité électrique</translate>,
     description: (
       <>
-        docs.bativolt.com vous accompagne pas à pas à travers les normes du RGIE, en expliquant chaque aspect des installations électriques. Avec des exemples concrets et des explications détaillées, nous vous aidons à garantir la conformité de vos projets.
+        <translate>docs.bativolt.com vous accompagne pas à pas à travers les normes du RGIE, en expliquant chaque aspect des installations électriques. Avec des exemples concrets et des explications détaillées, nous vous aidons à garantir la conformité de vos projets.</translate>
       </>
     ),
   },
   {
-    title: 'Ressource Pédagogique pour la Sécurité Électrique',
+    title: <translate>Ressource Pédagogique pour la Sécurité Électrique</translate>,
     Svg: require('@site/static/img/docs_bativolt_education_f8ru.svg').default,
-    altText: 'Image représentant une ressource pédagogique pour la sécurité électrique',
+    altText: <translate>Image représentant une ressource pédagogique pour la sécurité électrique</translate>,
     description: (
       <>
-        Plus qu'une simple documentation, notre site est une ressource pédagogique visant à éduquer et à assister les utilisateurs. Grâce aux dernières technologies, nous offrons une expérience de navigation fluide et fiable pour que vous puissiez vous concentrer sur l'essentiel : la sécurité et la conformité.
+        <translate>Plus qu'une simple documentation, notre site est une ressource pédagogique visant à éduquer et à assister les utilisateurs. Grâce aux dernières technologies, nous offrons une expérience de navigation fluide et fiable pour que vous puissiez vous concentrer sur l'essentiel : la sécurité et la conformité.</translate>
       </>
     ),
   },
   {
-    title: 'Mise en Relation avec des Professionnels Agréés',
+    title: <translate>Mise en Relation avec des Professionnels Agréés</translate>,
     Svg: require('@site/static/img/docs_bativolt_connecting_teams_re_hno7.svg').default,
-    altText: 'Image représentant la mise en relation avec des professionnels agréés',
+    altText: <translate>Image représentant la mise en relation avec des professionnels agréés</translate>,
     description: (
       <>
-        Trouvez facilement des agences agréées et des électriciens vérifiés et qualifiés. docs.bativolt.com vous met en relation avec des professionnels de confiance pour assurer la sécurité et la conformité de vos installations électriques.
+        <translate>Trouvez facilement des agences agréées et des électriciens vérifiés et qualifiés. docs.bativolt.com vous met en relation avec des professionnels de confiance pour assurer la sécurité et la conformité de vos installations électriques.</translate>
       </>
     ),
   },
   {
-    title: 'Interprétation des Normes et Infractions RGIE',
+    title: <translate>Interprétation des Normes et Infractions RGIE</translate>,
     Svg: require('@site/static/img/docs_bativolt_file_analysis_8k9b.svg').default,
-    altText: 'Image représentant l\'interprétation des normes et infractions RGIE',
+    altText: <translate>Image représentant l'interprétation des normes et infractions RGIE</translate>,
     description: (
       <>
-        Comprenez facilement les articles et infractions du RGIE grâce à notre guide détaillé. Que vous soyez un particulier ou un professionnel, docs.bativolt.com vous aide à interpréter les réglementations pour une conformité optimale.
+        <translate>Comprenez facilement les articles et infractions du RGIE grâce à notre guide détaillé. Que vous soyez un particulier ou un professionnel, docs.bativolt.com vous aide à interpréter les réglementations pour une conformité optimale.</translate>
       </>
     ),
   },
   {
-    title: 'Amélioration Continue de l’Expérience Utilisateur',
+    title: <translate>Amélioration Continue de l’Expérience Utilisateur</translate>,
     Svg: require('@site/static/img/docs_bativolt_innovative_re_rr5i.svg').default,
-    altText: 'Image représentant l\'amélioration continue de l\'expérience utilisateur',
+    altText: <translate>Image représentant l'amélioration continue de l'expérience utilisateur</translate>,
     description: (
       <>
-        Notre plateforme évolue constamment pour répondre aux besoins des utilisateurs et s’adapter aux nouvelles normes. Profitez de mises à jour régulières et de nouvelles fonctionnalités pour un support toujours plus complet.
+        <translate>Notre plateforme évolue constamment pour répondre aux besoins des utilisateurs et s’adapter aux nouvelles normes. Profitez de mises à jour régulières et de nouvelles fonctionnalités pour un support toujours plus complet.</translate>
       </>
     ),
   },
 ];
+
 
 function Feature({ Svg, title, description, altText }) {
   return (
